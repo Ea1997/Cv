c988c503c1f77012ca06ea093c5a5137199f57b1
unknown <salim.elazhari@gmail.com>
2020-06-13 19:22:06 +0000

    Updates

---
+++ precache-manifest.a202a7c98613774a1ed1ece490ca82e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b422cf4a24331e839da7c3d2469cd417",
+    "url": "/Cv/index.html"
+  },
+  {
+    "revision": "6237b60d8e5a21bc6a7a",
+    "url": "/Cv/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4a2dfbbe0329bb78c762",
+    "url": "/Cv/static/css/main.fbbfff8d.chunk.css"
+  },
+  {
+    "revision": "6237b60d8e5a21bc6a7a",
+    "url": "/Cv/static/js/2.1117318a.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Cv/static/js/2.1117318a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2dfbbe0329bb78c762",
+    "url": "/Cv/static/js/main.f513bbc8.chunk.js"
+  },
+  {
+    "revision": "6c6e767f0a7ee1a49aa9",
+    "url": "/Cv/static/js/runtime-main.faa8b65a.js"
+  }
+]);
